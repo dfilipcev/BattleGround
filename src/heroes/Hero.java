3ad03df8c296a02e613816799117c1098f5b45a5
Dmitriy Filiptsev <robertwertx@gmail.com>
2024-06-30 20:56:49 +0300

    add hero and training ground

---
+++ src/heroes/Hero.java
@@ -0,0 +1,18 @@
+package heroes;
+
+public class Hero {
+
+    private String name;
+
+    public Hero(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void attackEnemy() {
+        System.out.println("Hero is attacking enemy!");
+    }
+}
---
+++ src/trainingGround/TrainingGround.java
@@ -0,0 +1,13 @@
+package trainingGround;
+
+import heroes.Hero;
+
+public class TrainingGround {
+
+    public static void main(String[] args) {
+
+        Hero hero = new Hero("Hero");
+        hero.attackEnemy();
+
+    }
+}
